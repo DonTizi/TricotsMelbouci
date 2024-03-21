88a934d1fc2bd0d940f609a17b79db72f56aaba0
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2024-03-21 16:04:22 -0400

    test
    test

--- pngtojpg.py
+++ pngtojpg.py
@@ -25,9 +25,9 @@ dossier_courant = os.getcwd()
 # Parcourir tous les fichiers du dossier
 for fichier in os.listdir(dossier_courant):
   # Vérifier si le fichier est un PNG
-  if fichier.endswith(".png"):
+  if fichier.lower().endswith(".JPG"):
     # Déterminer le nom du fichier JPG
-    fichier_jpg = fichier.replace(".JPG", ".jpg")
+    fichier_jpg = fichier[:-4] + ".jpg"
 
     # Convertir le fichier PNG en JPG
     convertir_png_en_jpg(fichier, fichier_jpg)
@@ -36,4 +36,5 @@ for fichier in os.listdir(dossier_courant):
     print(f"Le fichier {fichier} a été converti en {fichier_jpg}")
 
 
-convertir_png_en_jpg("public/2.JPG", "public/test")
+
+convertir_png_en_jpg("/Users/dontizi/Downloads/tailwindui-studio/studio-js/public/items/2.JPG", "public/test")
