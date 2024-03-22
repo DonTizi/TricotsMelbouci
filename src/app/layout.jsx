1a665790a8c2d12b4e3af047f07cdf59fcb2f850
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2024-03-22 16:08:18 -0400

    SpeedInshightV2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,8 @@ export default function Layout({ children }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
-        <RootLayout>{children}</RootLayout>
+        <RootLayout>{children}
+        <SpeedInsights/></RootLayout>
       </body>
     </html>
   )
